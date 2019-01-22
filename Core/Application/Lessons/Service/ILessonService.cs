5d58c39be4fba53c9929b8e497ef82c0a540ac30
Dimitris <jimtsikosb@hotmail.gr>
2019-01-22 14:39:23 +0200

    Revert "Result handler added. Small modifications on lessons so that they can work. Modifications needed!!!"
    This reverts commit cc88e39736e69be42b476169abac5e72f4da2648.

--- Core/Application/Lessons/Service/ILessonService.cs
+++ Core/Application/Lessons/Service/ILessonService.cs
@@ -1,5 +1,4 @@
-﻿using Application.Handlers;
-using Application.Lessons.Dtos;
+﻿using Application.Lessons.Dtos;
 using System;
 using System.Collections.Generic;
 
@@ -12,20 +11,20 @@ namespace Application.Lessons.Service
         /// </summary>
         /// <param name="lessonId">The lesson id</param>
         /// <returns>The lesson DTO</returns>
-        ResultHandler<LessonDto> Get(Guid lessonId);
+        LessonDto Get(Guid lessonId);
 
         /// <summary>
         /// Get a list of all lessons
         /// </summary>
         /// <returns>The list of lessons</returns>
-        ResultHandler<IEnumerable<LessonDto>> GetList();
+        IEnumerable<LessonDto> GetList();
 
         /// <summary>
         /// Get a list of lessons by date
         /// </summary>
         /// <param name="dateTime">The datetime to search for</param>
         /// <returns>The list of lessons for a specific date</returns>
-        ResultHandler<IEnumerable<LessonDto>> GetByDate(DateTime dateTime);
+        IEnumerable<LessonDto> GetByDate(DateTime dateTime);
 
         /// <summary>
         /// Get a list of lessons by date and time
@@ -33,25 +32,25 @@ namespace Application.Lessons.Service
         /// <param name="dateTime">The datetime to search for</param>
         /// <param name="hour">The hour to search for</param>
         /// <returns>The list of lessons for a specific date and time</returns>
-        ResultHandler<IEnumerable<LessonDto>> GetByDateAndTime(DateTime dateTime, string hour);
+        IEnumerable<LessonDto> GetByDateAndTime(DateTime dateTime, string hour);
 
         /// <summary>
         /// Create a lesson
         /// </summary>
         /// <param name="LessonDto">The lesson DTO to be created</param>
         /// <returns>The lesson DTO</returns>
-        ResultHandler<LessonDto> Create(LessonDto lessonDto);
+        LessonDto Create(LessonDto lessonDto);
 
         /// <summary>
         /// Updates a lesson
         /// </summary>
         /// <param name="LessonDto">The lesson DTO to be updated</param>
-        ResultHandler<LessonDto> Update(LessonDto lessonDto);
+        void Update(LessonDto lessonDto);
 
         /// <summary>
         /// Deletes a lesson
         /// </summary>
         /// <param name="lessonId">The lesson id to be deleted</param>
-        ResultHandler<LessonDto> Delete(Guid lessonId);
+        void Delete(Guid lessonId);
     }
 }
